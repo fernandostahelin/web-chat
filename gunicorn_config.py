aa09816df3929914aedef053d8c09307ea30560f
Fernando Stahelin <fernandostahelin@gmail.com>
2024-09-24 14:48:34 -0300

    trying gunicorn

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,19 +1,16 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables
+
 load_dotenv()
 
-# Gunicorn configuration
+
 bind = f"{os.getenv('FLASK_HOST', '0.0.0.0')}:{os.getenv('FLASK_PORT', '8000')}"
 workers = 1
 worker_class = 'eventlet'
-wsgi_app = 'run:socketio'  # Changed from 'run:app' to 'run:socketio'
-
-# Enable threading
 threads = 4
 
-# Logging
+
 accesslog = '-'
 errorlog = '-'
 loglevel = 'info'
