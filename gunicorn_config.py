556b27b52e1f409271c4acd2e97b6e7fa3fb135c
Fernando Stahelin <fernandostahelin@gmail.com>
2024-09-24 14:30:19 -0300

    trying gunicorn

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Gunicorn configuration
-bind = f"{os.getenv('FLASK_HOST', '0.0.0.0')}:{os.getenv('FLASK_PORT', '5000')}"
+bind = f"{os.getenv('FLASK_HOST', '0.0.0.0')}:{os.getenv('FLASK_PORT', '8000')}"
 workers = 1
 worker_class = 'eventlet'
 wsgi_app = 'run:app'
